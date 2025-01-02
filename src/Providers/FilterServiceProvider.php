0e643092d58663e8cad85fd2d43b9abd8ecb72e5
Jorge Kania <jorge.kania@inovanti.com.br>
2025-01-02 09:03:12 -0300

    feat: first commit

---
+++ src/Providers/FilterServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Inovanti\AdvancedQueryFilters\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class FilterServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Registrar os serviços e bindings
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Configurações de boot
+    }
+}
